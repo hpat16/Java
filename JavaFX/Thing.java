b82ca7b26e9dc5e92f59daaaf669ab09250df52b
hpatel <158798743+hpat16@users.noreply.github.com>
2024-02-28 21:13:53 -0600

    Update Thing.java

--- JavaFX/Thing.java
+++ JavaFX/Thing.java
@@ -1,37 +1,9 @@
-//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
-//
-// Title:    Thing
-// Course:   CS 300 Spring 2023
-//
-// Author:   Henish Patel
-// Email:    hpatel37@wisc.edu
-// Lecturer: Hobbes LeGault
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name:    (name of your pair programming partner)
-// Partner Email:   (email address of your programming partner)
-// Partner Lecturer's Name: (name of your partner's lecturer)
-//
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-//   ___ Write-up states that pair programming is allowed for this assignment.
-//   ___ We have both read and understand the course Pair Programming Policy.
-//   ___ We have registered our team prior to the team registration deadline.
-//
-///////////////////////// ALWAYS CREDIT OUTSIDE HELP //////////////////////////
-//
-// Persons:         NONE
-// Online Sources:  NONE
-//
-///////////////////////////////////////////////////////////////////////////////
-
 import java.io.File;
 import processing.core.PImage;
 
 /**
  * This class models a graphic Thing which can be drawn at a give (x,y) position within the display
  * window of a graphic application.
- * @author henishpatel
  */
 public class Thing {
   // PApplet object that represents the display window of this graphic application
